9c3a429a677a50a12cf4cd1f64d62285c053c1f5
PuRp <chelovechina09@gmail.com>
2025-10-08 13:48:58 +0200

    Lab_3 finally

--- Lab_3/Lab_3/Lab_3.cpp
+++ Lab_3/Lab_3/Lab_3.cpp
@@ -9,31 +9,36 @@ int main() {
     int N;
     int sum = 0, mx = 0, num = 0;
     cin >> N;
-    for (int i = 1; i <= N; ++i) {
-        int a;
-        cin >> a;
-        if (a % 3 != 0 && a % 5 != 0 && a % 7 != 0) {
-            sum += a;
-            mx = mx <= a ? a : mx;
-            num = mx == a ? i : num;
+    if (N != 0) {
+        for (int i = 1; i <= N; ++i) {
+            int a;
+            cin >> a;
+            if (a % 3 != 0 && a % 5 != 0 && a % 7 != 0) {
+                sum += a;
+                mx = mx <= a ? a : mx;
+                num = mx == a ? i : num;
+            }
+        }
+        if (mx == 0) {
+            cout << "В последовательности не найдены подходящие значения" << endl << endl;
+        }
+        else {
+            cout << "Сумма значений последовательности: " << sum << endl;
+            cout << "Максимальное значение последовательности: " << mx << endl;
+            cout << "Номер максимального значения: " << num << endl << endl;
         }
     }
-    if (sum == 0) {
-        cout << "В последовательности не найдены подходящие значения" << endl << endl;
-    }
-    else {
-        cout << "Сумма значений последовательности: " << sum << endl;
-        cout << "Максимальное значение последовательности: " << mx << endl;
-        cout << "Номер максимального значения: " << num << endl << endl;
-    }
+    else cout << "Задана пустая последовательность!" << endl;
+
 
     // Задание 2
     int n;
-    cin >> n;
-    if (abs(n) >= 1000) {
-        cout << "Некорректное число";
-        return 0;
-    }
+    do {
+        cin >> n;
+        if (abs(n) >= 1000) {
+            cout << "Введите корректное число (по модулю меньше 1000)!" << endl;
+        }
+    } while (abs(n) >= 1000);
 
     int copy_n = n;
     int count = 0;
@@ -42,18 +47,9 @@ int main() {
         n /= 10;
     } while (n);
 
-    if (count == 3) {
-        cout << "Кол-во индексов: " << count << endl;
-        cout << "Первая цифра: " << abs(copy_n) / 100;
-    }
-    else if (count == 2) {
-        cout << "Кол-во индексов: " << count << endl;
-        cout << "Первая цифра: " << abs(copy_n) / 10;
-    }
-    else {
-        cout << "Кол-во индексов: " << count << endl;
-        cout << "Первая цифра: " << abs(copy_n);
-    }
+    cout << "Кол-во разрядов: " << count << endl;
+    cout << "Первая цифра: " << abs(copy_n) / pow(10, count - 1);
+    
 
     return 0;
 }
