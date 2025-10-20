bdde39627c56983942a948f4088fd886b258709a
PuRp <chelovechina09@gmail.com>
2025-10-20 22:44:46 +0200

    Lab_4

---
+++ Lab_4/Lab_4.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+// Задание №1
+    const int N = 9;
+    int arr[N];
+    bool flag = false;
+    // Заполнение массива
+    for (int i = 0; i < N; ++i) {
+        std::cin >> arr[i];
+        // Нахождение в массиве числа, начинающегося с цифры 3
+        if (!flag) {
+            int i_from_arr = abs(arr[i]);
+            int maybe_3;
+            while (i_from_arr) {
+                maybe_3 = i_from_arr % 10;
+                i_from_arr /= 10;
+            }
+            if (maybe_3 == 3) {
+                flag = true;
+            }
+        }
+    }
+    // Упорядочивание массива по возрастанию
+    if (flag) {
+        for (int i = 0; i < N - 1; ++i) {
+            for (int j = i + 1; j < N; ++j) {
+                if (arr[i] > arr[j]) {
+                    std::swap(arr[i], arr[j]);
+                }
+            }
+        }
+        // Вывод полученного массива
+        for (int i = 0; i < N; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    else std::cout << "В массиве не найдены числа, начинающиеся с тройки => сортировка невозможна!" << std::endl << std::endl;
+
+//--------------------------------------------------------------
+
+// Задание №2
+    const int I = 3, J = 4;
+    int mat[I][J];
+    // Заполнение матрицы
+    for (int i = 0; i < I; ++i) {
+        for (int j = 0; j < J; ++j) {
+            std::cin >> mat[i][j];
+        }
+    }
+    // Поиск строки с максимальным значением суммы элементов
+    int max_sum = 0;
+    int num_line = 0;
+    for (int i = 0; i < I; ++i) {
+        int sum_of_lines = 0;
+        for (int j = 0; j < J; ++j) {
+            sum_of_lines += mat[i][j];
+        }
+        if (max_sum < sum_of_lines) {
+            max_sum = sum_of_lines;
+            num_line = i;
+        }
+    }
+    // Замена в найденной строке всех элементов на 999
+    for (int j = 0; j < J; ++j) {
+        mat[num_line][j] = 999;
+    }
+    // Вывод полученной матрицы
+    for (int i = 0; i < I; ++i) {
+        for (int j = 0; j < J; ++j) {
+            std::cout << mat[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
