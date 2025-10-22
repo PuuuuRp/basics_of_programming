2f27f9b879190b0aa1c3e971f038d6a3cc93eaeb
PuRp <chelovechina09@gmail.com>
2025-10-22 12:34:26 +0200

    Lab_4_1 fin

--- Lab_4_1/Lab_4_1/Lab_4_1.cpp
+++ Lab_4_1/Lab_4_1/Lab_4_1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <limits>
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -6,21 +7,21 @@ int main() {
     // Задание №1
     const int N = 9;
     int arr[N];
-    bool flag = false;
     // Заполнение массива
     for (int i = 0; i < N; ++i) {
         std::cin >> arr[i];
-        // Нахождение в массиве числа, начинающегося с цифры 3
-        if (!flag) {
-            int i_from_arr = abs(arr[i]);
-            int maybe_3;
-            while (i_from_arr) {
-                maybe_3 = i_from_arr % 10;
-                i_from_arr /= 10;
-            }
-            if (maybe_3 == 3) {
-                flag = true;
-            }
+    }
+    // Нахождение в массиве числа, начинающегося с цифры 3
+    bool flag = false;
+    for (int i = 0; i < N; ++i) {
+        int i_from_arr = abs(arr[i]);
+        int maybe_3;
+        while (i_from_arr) {
+            maybe_3 = i_from_arr % 10;
+            i_from_arr /= 10;
+        }
+        if (maybe_3 == 3) {
+            flag = true;
         }
     }
     // Упорядочивание массива по возрастанию
@@ -53,20 +54,23 @@ int main() {
     }
     // Поиск строки с максимальным значением суммы элементов
     int max_sum = std::numeric_limits<int>::min();
-    int num_line = 0;
+    int num_line = -1;
     for (int i = 0; i < N_mat; ++i) {
         int sum_of_lines = 0;
         for (int j = 0; j < M_mat; ++j) {
             sum_of_lines += mat[i][j];
         }
-        if (max_sum < sum_of_lines) {
-            max_sum = sum_of_lines;
+        if (max_sum < abs(sum_of_lines)) {
+            max_sum = abs(sum_of_lines);
             num_line = i;
         }
     }
+
     // Замена в найденной строке всех элементов на 999
-    for (int j = 0; j < M_mat; ++j) {
-        mat[num_line][j] = 999;
+    if (num_line >= 0 && num_line < N_mat) {
+        for (int j = 0; j < M_mat; ++j) {
+            mat[num_line][j] = 999;
+        }
     }
     // Вывод полученной матрицы
     for (int i = 0; i < N_mat; ++i) {
---
+++ Lab_5/Lab_5.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+
+void ReadArr(int n, int arr[]) {
+    std::cout << "Исп. ф-ия для ввода данных для массива." << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+    std::cout << std::endl;
+}
+
+void WriteArr(int n, int arr[]) {
+    std::cout << "Исп. ф-ия для вывода массива." << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+bool ChechNumers(int n, int arr[]) {
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    
+    int n = 11;
+    while (n < 1 || n > 10) {
+        std::cout << "Введите, сколько значений будет вводиться (от 1 до 10)." << std::endl;
+        std::cin >> n;
+    }
+    int arr[10];
+    ReadArr(n, arr);
+
+
+    return 0;
+}
