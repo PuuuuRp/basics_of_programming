63e44df580de03b0d67a2d9516c181d1e3a5e93e
PuRp <chelovechina09@gmail.com>
2025-11-18 19:52:01 +0200

    Какие-то тестовые примеры

---
+++ Examples/Examples.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+
+    int* arr = (int*)calloc(2, sizeof(int));
+
+    std::cout << sizeof(arr) << std::endl;
+    std::cout << sizeof(*arr) << std::endl;
+
+    free(arr);
+
+    return 0;
+}
